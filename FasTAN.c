0368626679177e00e4d4a08391d214e96c6b1ac0
Anant Maheshwari <anant.maheshwari.76@gmail.com>
2026-04-09 14:47:24 +0100

    fix double free mult-threading bug

--- FasTAN.c
+++ FasTAN.c
@@ -752,7 +752,7 @@ int main(int argc, char *argv[])
         if (i == 0)
           Free_Align_Spec(parm[i].spec);
         else
-          fclose(_gdb.seqs);
+          fclose(parm[i]._gdb.seqs);
         Free_Work_Data(parm[i].work);
       }
 
